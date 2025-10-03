87487b80273bfc4e6423632a5b28a84ad0191a3f
Nicolas Lundin <nlundin@gmail.com>
2025-10-03 00:43:21 -0300

    Updates

--- assets/CaseStudyLayout-UlMjzZuu.js
+++ assets/CaseStudyLayout-UlMjzZuu.js
@@ -1,4 +1,4 @@
-import{c as g,r as u,j as e,u as v,m as t}from"./index-DZVXFp1U.js";/**
+import{c as g,r as u,j as e,u as v,m as t}from"./index-Q2_6K3f6.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
