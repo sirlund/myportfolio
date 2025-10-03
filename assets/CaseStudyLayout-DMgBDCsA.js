71671ae60f637db166243608733423dba1321d9d
Nicolas Lundin <nlundin@gmail.com>
2025-10-03 10:30:37 -0300

    Updates

--- assets/CaseStudyLayout-DMgBDCsA.js
+++ assets/CaseStudyLayout-DMgBDCsA.js
@@ -1,4 +1,4 @@
-import{c as g,r as u,j as e,u as v,m as t}from"./index-Q2_6K3f6.js";/**
+import{c as g,r as u,j as e,u as v,m as t}from"./index-BflXGsym.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
